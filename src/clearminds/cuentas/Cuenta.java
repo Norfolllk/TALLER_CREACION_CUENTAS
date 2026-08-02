a3c15e81356a8a75fde4a5befd38a1fe7ef679b2
Norfolllk <alexander.garofalo.ag@gmail.com>
2026-08-01 19:30:38 -0500

    Agrego "Cuenta y TestCuenta" ya modificado con encapsulamiento, constructores y adicionales

--- src/clearminds/cuentas/Cuenta.java
+++ src/clearminds/cuentas/Cuenta.java
@@ -2,4 +2,58 @@ package clearminds.cuentas;
 
 public class Cuenta {
 
+	private String id;
+	private String tipo;
+	private double saldo;
+
+	// Constructor de ID: recibe el id y asigna el tipo "A" automáticamente
+	public Cuenta(String id) {
+		this.id = id;
+		this.tipo = "A";
+	}
+
+	// Constructor Completo: recibe id, tipo y saldo
+	public Cuenta(String id, String tipo, double saldo) {
+		this.id = id;
+		this.tipo = tipo;
+		this.saldo = saldo;
+	}
+
+	// Solo get, no set: el id no se puede cambiar una vez creada la cuenta
+	public String getId() {
+		return id;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+	public double getSaldo() {
+		return saldo;
+	}
+
+	public void setSaldo(double saldo) {
+		this.saldo = saldo;
+	}
+
+	public void imprimir() {
+		System.out.println("           CUENTA");
+		System.out.println("************************");
+		System.out.println("Número de Cuenta: " + id);
+		System.out.println("Tipo: " + tipo);
+		System.out.println("Saldo: USD " + saldo);
+		System.out.println("************************");
+	}
+
+	public void imprimirConMiEstilo() {
+		System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+		System.out.println("💳 Cuenta N° " + id + " (Tipo " + tipo + ")");
+		System.out.println("💰 Saldo disponible: $" + saldo + " USD");
+		System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+	}
+
 }
--- src/clearminds/cuentas/test/TestCuenta.java
+++ src/clearminds/cuentas/test/TestCuenta.java
@@ -1,9 +1,53 @@
 package clearminds.cuentas.test;
 
+import clearminds.cuentas.Cuenta;
+
 public class TestCuenta {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		// 1. Creación de instancias con diferentes constructores
+		Cuenta cuenta1 = new Cuenta("03476");
+		cuenta1.setSaldo(675);
+		Cuenta cuenta2 = new Cuenta("03476", "C", 98);
+		Cuenta cuenta3 = new Cuenta("03476");
+		cuenta3.setTipo("C");
+
+		// 2. Visualización inicial
+		System.out.println("-------- Valores Iniciales ---------");
+		cuenta1.imprimir();
+		cuenta2.imprimir();
+		cuenta3.imprimir();
+
+		// --- Reto Adicional ---
+
+		// Modificamos saldos y tipos para verificar el encapsulamiento
+		System.out.println("\n-------- Después de Modificar --------");
+		cuenta1.setSaldo(1200);
+		cuenta1.setTipo("B");
+		cuenta1.imprimir();
+
+		cuenta2.setSaldo(50);
+		cuenta2.imprimir();
+
+		// Tres cuentas adicionales explorando los constructores sobrecargados
+		System.out.println("\n-------- Cuentas Adicionales --------");
+
+		Cuenta cuenta4 = new Cuenta("10001");
+		cuenta4.imprimir();
+
+		Cuenta cuenta5 = new Cuenta("10002", "B", 1500.50);
+		cuenta5.imprimir();
+
+		Cuenta cuenta6 = new Cuenta("10003");
+		cuenta6.setTipo("C");
+		cuenta6.setSaldo(320.75);
+		cuenta6.imprimir();
+
+		// Probando el método con estilo personalizado
+		System.out.println("\n-------- Estilo Personalizado --------");
+		cuenta1.imprimirConMiEstilo();
+		cuenta5.imprimirConMiEstilo();
 
 	}
 
